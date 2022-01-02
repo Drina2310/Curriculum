e021ca713a781ef6eb57d631203e6e187d1b7a17
Drina_Rincon <drina.rincon@bitlogic.io>
2022-01-02 17:50:22 -0300

    componente about-me y curriculum

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import BasicLayout from '../layouts/BasicLayout';
 import Profile from '../components/Profile';
+import AboutMe from '../components/AboutMe'
 import "./index.scss";
 
 export default function Index() {
   return (
     <BasicLayout>
       <Profile />
-      <h1 className="title">Estamos en la home</h1>
+      <AboutMe />
     </BasicLayout>
   )
 }
