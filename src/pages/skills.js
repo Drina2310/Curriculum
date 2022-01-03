0d0fb743ed7a7b8364df4cfe2793c3a1f4873004
Drina_Rincon <drina.rincon@bitlogic.io>
2022-01-03 12:33:20 -0300

    creando componente para mostrar skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
 import BasicLayout from '../layouts/BasicLayout';
-import "./index.scss";
+import ListSkills from '../components/ListSkills'
+import "./skills.scss";
+
+const skills = [
+    {type: "Java", level: 85},
+    {type: "Javascript", level: 75},
+]
 
 export default function Skills() {
     return (
         <BasicLayout menuColor="#000">
-            <h2 className="title">Estamos en skills</h2>
+            <Container className="skills">
+                <div className="skills__block">
+                    <h2>Frontend</h2>
+                    <ListSkills skills={skills}/>
+                </div>
+            </Container>
         </BasicLayout>
     )
 }
